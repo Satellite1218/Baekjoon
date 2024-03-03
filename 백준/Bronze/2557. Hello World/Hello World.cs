4e9fd3a2ea8aa4e017edd97c9dd00f4876f7ad69
이신우 <128461193+Satellite1218@users.noreply.github.com>
2024-03-03 23:27:24 +0900

    [Bronze V] Title: Hello World, Time: 64 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Xml.Linq;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
