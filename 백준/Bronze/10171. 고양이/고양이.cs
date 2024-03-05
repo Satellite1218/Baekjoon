25608b799262ff4a0fe6a69068d0567afe2bcffe
이신우 <128461193+Satellite1218@users.noreply.github.com>
2024-03-05 22:51:56 +0900

    [Bronze V] Title: 고양이, Time: 56 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Xml.Linq;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
